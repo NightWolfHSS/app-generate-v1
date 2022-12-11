82cddb2d1273c8c93dbab526a7a1d9002be9e110
Slavik <nice.kirat@mail.ru>
2022-12-12 00:14:22 +0600

    first

---
+++ main.js
@@ -0,0 +1,46 @@
+const base = (function() {
+	let hash = '02102101120221021101210101212210121'
+	let fName = "Slavik"
+	//
+	function bigIdea() {
+		return 'тут кто то есть ?'
+	}
+	//
+	function renderHssCodeBidSoult(hash) {
+		return hash
+	}
+
+	bidData = {
+		good: "my",
+		love: "false",
+		life: "death",
+		age: "23 str",
+		mix: [
+		    "12",
+		    "firstname",
+		    bigIdea.age
+		]
+	}
+	//
+	return {
+		hashShhl: renderHssCodeBidSoult(hash),
+	    nameFunc: bigIdea(),
+	    fname: fName,
+	    data: bidData
+	}
+
+
+})()
+
+console.log(base.nameFunc)
+console.log(base.fname)
+console.log(base.hashShhl)
+console.log(base.data)
+// сделай массив что бы буквы были все большие
+// функция которая смешает парол вернет данные ?
+// функция которая сгенерирует  получит и смешает = кнопка  - усложнить | сгенерировать новый | удалить
+// отдельные функции которые что то за что то отвечают
+{
+  const base = "new dir new life new feild new place"
+  console.log(base)
+}
